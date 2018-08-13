d2c4ca68f9536672f3a97b6ebd66e2489a554ef7
MissPeperr <madisonpeper@gmail.com>
2018-08-13 16:06:00 -0500

    added the require fo taskDOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
 const manageUserData = require("./DataManager");
+const createTask = require("./taskDOM");
 
+createTask
 console.log(manageUserData.getData.getUsers());
--- src/scripts/task/taskDOM.js
+++ src/scripts/task/taskDOM.js
@@ -1,7 +1,10 @@
-const createTask = () => {
-    return `
-        <div id="task-div">
-            
-        </div>    
-    `
+const createTaskDOM = () => {
+    $("#task-div").append( `
+    <div id="task-container">
+        <h3>Tasks</h3>
+        <button>Add a new task!</button>
+    </div>    
+`)
 }
+
+module.exports = createTaskDOM;
