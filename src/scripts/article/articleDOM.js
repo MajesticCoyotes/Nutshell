420d59953caa4c6614fcc684d661dc75bb14e4a6
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-08-17 11:56:08 -0500

    fixed messages section errors

--- src/scripts/article/articleDOM.js
+++ src/scripts/article/articleDOM.js
@@ -6,7 +6,7 @@ const newArticleDOM = (article) => {
         <button type="button" id="deleteArticle--${article.id}" class="deleteArticleButton btn"><span class="fas fa-trash-alt deleteArticleIcon" id="deleteArticle--${article.id}"></span></button>
         <h4>Title: ${article.title}</h4>
         <p>Synopsis: ${article.synopsis}</p>
-        <p>URL: ${article.url}</p>
+        <p>URL: <a href="http://${article.url}">${article.url}</a></p>
         <hr>
         <p class="articleDate"><em>${article.date}</em></p>
     </div>
